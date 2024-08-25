25da077030d26edc351088d797e1d9f95f56ca81
Anate Muhammed <anatemuhammed@gmail.com>
2024-08-25 19:34:42 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//circle 1
+document.getElementById("circle1").onclick = function () {
+  document.getElementById("circle1").style.display = "none";
+};
+
+//circle 2
+document.getElementById("circle2").onclick = function () {
+  document.getElementById("circle2").style.display = "none";
+};
+//circle 3
+document.getElementById("circle3").onclick = function () {
+  document.getElementById("circle3").style.display = "none";
+};
